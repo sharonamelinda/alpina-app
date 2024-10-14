dc8c52e68fac9b2ca7cf4ca5f5eb768e4ae72416
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-10-14 16:41:39 +0700

    Update after revision

--- public/js/swipe.js
+++ public/js/swipe.js
@@ -2,6 +2,6 @@
 const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: false,
-    slidesPerView: "auto", // Adjust to show multiple slides based on content width
-    spaceBetween: 10, // Space between slides
-  });
+    slidesPerView: "auto",
+    freeMode: true
+})
