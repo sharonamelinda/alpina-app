225c23f07adc42de9fc1bc844755e67493582c1d
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-10-14 13:19:33 +0700

    Add swiper.js

---
+++ public/js/swipe.js
@@ -0,0 +1,7 @@
+// Initialize Swiper
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: "auto", // Adjust to show multiple slides based on content width
+    spaceBetween: 10, // Space between slides
+  });
